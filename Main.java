e3879cf5a070c0375d437fb82a91ecfbe1e7c76b
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-11 21:26:16 -0600

    Update and rename README.md to Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+/* Started at 8:11 2-11-2022 */
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter a String");
+    String msg = scan.nextLine();
+
+    System.out.println("Enter an Integer");
+    int num = scan.nextInt();
+
+    System.out.println("Enter a Double");
+    double num2 = scan.nextDouble();
+
+    System.out.println(msg);
+    System.out.println(num);
+    System.out.println(num2);
+  }
+} 
+
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter your name");
+    String msg = scan.nextLine();
+
+    System.out.println("HI " + msg);
+  }
+}
+
+// Ended coding java at 9:25 I'm now going onto JAVASCRIPT
